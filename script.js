f27d6bca45a227c03e8a6ac9117e76a698164393
ishayashika <122111684+ishayashika@users.noreply.github.com>
2025-02-01 18:16:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+let fname=document.getElementsByName("fname")[0];
+let lname=document.getElementsByName("lname")[0];
 function getFormvalue() {
     //Write your code here
+	let first=fname.value;
+	let last=lname.value;
+	alert(first+" "+last);
+	
 
 }
